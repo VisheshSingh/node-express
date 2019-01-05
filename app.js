fa936420e785a20759ba702a8041c05b9bbb1596
Vishesh <visheshsingh16@gmail.com>
2019-01-05 13:46:22 -0500

    Mongoose and model setup

--- app.js
+++ app.js
@@ -1,37 +1,38 @@
 const express = require('express');
 const path = require('path');
+const mongoose = require('mongoose');
 
 const app = express();
+
+mongoose.connect("mongodb://localhost/nodekb");
+let db = mongoose.connection;
+
+db.once('open', () => {
+    console.log('MongoDB Connected...');
+})
+
+db.on('error', (err) => {
+    console.log(err);
+})
+
+let Articles = require('./models/article');
 
 // load view engine
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug')
 
 app.get("/", (req, res) => {
-    let articles = [
-        {
-            id: 1,
-            title: 'Article one',
-            author: 'Mr X',
-            body: 'This is body one'
-        },
-        {
-            id: 2,
-            title: 'Article two',
-            author: 'Mr Y',
-            body: 'This is body two'
-        },
-        {
-            id: 3,
-            title: 'Article three',
-            author: 'Mr Z',
-            body: 'This is body three'
+    Articles.find({}, (err, articles) => {
+        if (err) {
+            console.log(err);
+        } else {
+
+            res.render('index', {
+                title: 'Articles',
+                articles: articles
+            });
         }
-    ]
-    res.render('index', {
-        title: 'Articles',
-        articles: articles
-    });
+    })
 })
 
 app.get("/articles/add", (req, res) => {
